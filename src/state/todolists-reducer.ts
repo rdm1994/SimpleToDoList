8585b4dcf1052aa70618d8e177516fd04c91449b
Dim <russudim1994@gmail.com>
2021-10-06 18:27:14 +0300

    create new .ts file "todolists-reducer" for reducer

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,50 @@
+import { TodoListType, FilterValuesType } from './../App';
+import { v1 } from 'uuid';
+
+type RemoveTodoListAT = {
+    type: "REMOVE-TODOLIST"
+    todoListID: string
+
+}
+type AddTodoListAT = {
+    type: "ADD-TODOLIST"
+    title: string
+
+}
+type ChangeTodoListTitleAT = {
+    type: "CHANGE-TODOLIST-TITLE"
+    title: string
+    todoListID: string
+
+}
+type ChangeTodoListFilterAT = {
+    type: "CHANGE-TODOLIST-FILTER"
+    todoListID: string
+    value: FilterValuesType
+
+}
+type ActionType = RemoveTodoListAT | AddTodoListAT | ChangeTodoListTitleAT | RemoveTodoListAT | ChangeTodoListFilterAT
+
+
+
+export const todoListsReducer = (todoLists: Array<TodoListType>, action: ActionType) => {
+    switch (action.type) {
+        case "REMOVE-TODOLIST":
+            return todoLists.find(tl => tl.id === action.todoListID)
+        case "ADD-TODOLIST":
+            const newTodoListID = v1()
+            const newTodoList: TodoListType = {
+                id: newTodoListID, title: action.title, filter: "all"
+            }
+            return [...todoLists, newTodoList]
+        case "CHANGE-TODOLIST-TITLE":
+            return todoLists.map(tl => tl.id === action.todoListID ? { ...tl, title: action.title } : tl)
+        case "CHANGE-TODOLIST-FILTER":
+            return todoLists.map(tl => tl.id === action.todoListID ? { ...tl, filter: action.value } : tl)
+
+        default:
+            return todoLists
+    }
+
+}
+
