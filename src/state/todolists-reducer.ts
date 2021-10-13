051715882239848aaa8328e17bbd17dcb28c216a
Dim <russudim1994@gmail.com>
2021-10-13 19:10:01 +0300

    add action creaters for each action

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,15 +1,15 @@
 import { TodoListType, FilterValuesType } from './../App';
 import { v1 } from 'uuid';
 
-type RemoveTodoListAT = {
+export type RemoveTodoListAT = {
     type: "REMOVE-TODOLIST"
     todoListID: string
 
 }
-type AddTodoListAT = {
+export type AddTodoListAT = {
     type: "ADD-TODOLIST"
     title: string
-
+    todoListID: string
 }
 type ChangeTodoListTitleAT = {
     type: "CHANGE-TODOLIST-TITLE"
@@ -32,7 +32,7 @@ export const todoListsReducer = (todoLists: Array<TodoListType>, action: ActionT
         case "REMOVE-TODOLIST":
             return todoLists.filter(tl => tl.id !== action.todoListID)
         case "ADD-TODOLIST":
-            const newTodoListID = v1()
+            const newTodoListID = action.todoListID
             const newTodoList: TodoListType = {
                 id: newTodoListID, title: action.title, filter: "all"
             }
@@ -47,7 +47,16 @@ export const todoListsReducer = (todoLists: Array<TodoListType>, action: ActionT
     }
 
 }
- // Action creater for example
-export const RemoveTodoListAC = (todoListID: string): RemoveTodoListAT => {
-    return {type: "REMOVE-TODOLIST",todoListID}
+
+export const removeTodoListAC = (todoListID: string): RemoveTodoListAT => {
+    return { type: "REMOVE-TODOLIST", todoListID }
+}
+export const addTodoListAC = (title: string): AddTodoListAT => {
+    return { type: "ADD-TODOLIST", title, todoListID: v1() }
+}
+export const changeTodoListTitleAC = (title: string, todoListID: string): ChangeTodoListTitleAT => {
+    return { type: "CHANGE-TODOLIST-TITLE", title, todoListID }
+}
+export const changeTodoListFilterAC = (todoListID: string, value: FilterValuesType): ChangeTodoListFilterAT => {
+    return { type: "CHANGE-TODOLIST-FILTER", todoListID, value }
 }
