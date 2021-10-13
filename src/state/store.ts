8073cf97cfb706845670dda7c6a7a1c27007e8d2
Dim <russudim1994@gmail.com>
2021-10-14 01:31:05 +0300

    created store

---
+++ src/state/store.ts
@@ -0,0 +1,18 @@
+import { tasksReducer } from "./tasks-reducer";
+import { combineReducers, createStore } from "redux";
+import { todoListsReducer } from "./todolists-reducer";
+
+
+
+const rootReducer = combineReducers({
+    todoLists: todoListsReducer,
+    tasks: tasksReducer
+})
+
+export type AppRootState = ReturnType<typeof rootReducer>
+
+export const store = createStore(rootReducer);
+
+
+// @ts-ignore
+window.store = store;
